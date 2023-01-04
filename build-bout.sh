0878e2973a2e9d976717158a334eb20551823d56
Mike Kryjak <mikekryjak@gmail.com>
2023-01-04 12:13:28 +0000

    works

--- build-bout.sh
+++ build-bout.sh
@@ -11,14 +11,14 @@ echo $PWD
 cd ../BOUT-966bde0
 
 # Get BOUT++ (replace branch with your branch of choice)
-# rm -rf BOUT-dev # Remove if already exists
-# git clone https://github.com/boutproject/BOUT-dev
+rm -rf BOUT-dev # Remove if already exists
+git clone https://github.com/boutproject/BOUT-dev
 cd BOUT-dev
-# git checkout 966bde0 # Hermes branch
+git checkout 966bde0 # Hermes branch
 
-# git submodule update --init --recursive
+git submodule update --init --recursive
 
 PETSC_DIR=$PWD/../dependencies/petsc-build PETSC_ARCH="" cmake . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS_RELEASE="-fast -DNDEBUG" -DCHECK=0 -DCMAKE_CXX_COMPILER=CC -DCMAKE_FTN_COMPILER=ftn -DBOUT_DOWNLOAD_SUNDIALS=ON -DBOUT_USE_PETSC=ON -DBOUT_IGNORE_CONDA_ENV=ON
 
-# cmake --build build -j 16
+cmake --build build -j 16
 
--- build-hermes3.sh
+++ build-hermes3.sh
@@ -7,9 +7,14 @@ exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>hermes3-buildlog.out 2>&1
 
-cd hermes-3
+branch="power-balance"
 
-cmake . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="/users/mjk557/scratch/BOUT-hermes3/BOUT-dev/build" -DHERMES_BUILD_BOUT=False
-cd build
+cd ../hermes-3
+git checkout $branch
+git pull
+rm -rf $branch
+
+cmake . -B $branch -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="/work/e281/e281/mkryjak/BOUT-966bde0/BOUT-dev/build" -DHERMES_BUILD_BOUT=False -DCMAKE_CXX_COMPILER=CC
+cd $branch
 make -j 8
 cd ..
